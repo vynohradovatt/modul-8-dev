3dd7a550ad2b6988789fc120672d5632f354b10e
Tanya Vynohradova <tanya.germanuk@gmail.com>
2023-08-10 13:43:25 +0300

    fix

--- src/main/java/org/example/TimeServlet.java
+++ src/main/java/org/example/TimeServlet.java
@@ -1,37 +1,68 @@
 package org.example;
 
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-
 
 @WebServlet(value = "/time")
 public class TimeServlet extends HttpServlet {
+    private TemplateEngine engine;
 
+    private String timezone;
+    @Override
+    public void init() {
+        engine = new TemplateEngine();
+
+        FileTemplateResolver resolver = new FileTemplateResolver();
+        resolver.setPrefix("/Users/Tanya/Downloads/modul-9-dev/src/main/templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML5");
+        resolver.setOrder(engine.getTemplateResolvers().size());
+        resolver.setCacheable(false);
+        engine.addTemplateResolver(resolver);
+    }
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         
         resp.setContentType("text/html; charset=utf-8");
-        resp.getWriter().write(parseTimezone(req));
-        resp.getWriter().close();
-    }
 
-    private String parseTimezone(HttpServletRequest request) {
+        String timezoneParam = req.getParameter("timezone");
 
-        String initTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        if( timezoneParam != null){
+            timezone = timezoneParam;
+            resp.addCookie(new Cookie("lastTimezone", timezone.replace(" ", "+")));
+        } else {
+            Cookie[] cookies = req.getCookies();
+            if(cookies != null) {
+                for (Cookie cookie : cookies) {
+                    if (cookie.getName().equals("lastTimezone")) {
+                        timezone = cookie.getValue();
+                    }
+                }
 
-        ZonedDateTime localDateTime = ZonedDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss 'UTC'XXX");
+            }
 
-        if (request.getParameterMap().containsKey("timezone")) {
-
-            return initTime + " " + request.getParameter("timezone").replace(' ', '+');
         }
-        return  localDateTime.format(formatter);
+
+        LocalDateTime localDateTime = LocalDateTime.now(ZoneId.of(timezone.replace(" ","+")));
+        String currentTimeFormatted = localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
+
+        Context context = new Context(req.getLocale());
+        context.setVariable("currentTime", currentTimeFormatted);
+
+        engine.process("time", context, resp.getWriter());
+
+        resp.getWriter().close();
     }
 }
--- src/main/java/org/example/TimezoneValidateFilter.java
+++ src/main/java/org/example/TimezoneValidateFilter.java
@@ -3,6 +3,7 @@ package org.example;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,19 +17,22 @@ public class TimezoneValidateFilter extends HttpFilter {
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
 
         String timezone = req.getParameter("timezone");
+
+        if (timezone == null) {
+            timezone = "UTC";
+        }
+
         if(timezone != null) {
             try {
                 ZoneId.of(timezone.replace(" ", "+"));
                 chain.doFilter(req, res);
             } catch (DateTimeException e) {
-
                 res.setStatus(400);
                 res.setContentType("application/json");
                 res.getWriter().write("Invalid timezone");
                 res.getWriter().close();
             }
-        } else {
-            new TimeServlet().doGet(req, res);
         }
+
     }
 }
